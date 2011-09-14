e60c7ee775b9912031d90e03c86839554b72855e
Diego Marcos <diego.marcos@gmail.com>
2011-09-13 17:56:29 -0700

    Improving record parser

--- fits.js
+++ fits.js
@@ -1,3 +1,5 @@
+// FITS Standard 3.0
+
 (function(){
 
   // Save a reference to the global object.
@@ -30,68 +32,48 @@
     };
   }
   
-  function parseHeaderRecord(recordString){
+  function parseHeaderRecord(recordString, error, warning){
     var record = {};
     // Replace multiple spaces by a single one
-    recordString = recordString.replace(/\s{2,}/g, ' ');
-    if(/^COMMENT/.test(recordString)){
-      record.comment = recordString.replace(/^COMMENT/,'');
-      return record;
+    var keyWord = recordString.substring(0, 8); // Sec 4.1.2.1
+    keyWord = keyWord.trim();
+    if(!/^[\x30-\x39\x41-\x5A\x5F\x2D]+$/.test(keyWord)){ // Allowed characters in keyword Sec 4.1.2.1
+      error("Illegal characther in header keyWord: " + keyWord);
     }
-    recordString = recordString.split(/ \/ /);
-    if(recordString.length > 2){
-      console.log("Malformed record");
-      return;
-    }
-    // Trim spaces at beggining and end
-    if(!/\s*/.test(recordString[1])){
-      record.comment = recordString[1].trim();
-    }
-    recordString = recordString[0].split(/=/);
-    if(recordString.length != 2){
-      console.log("Malformed record");
-      return;
-    }
-    // Remove spaces from id and value
-    recordString[0] = recordString[0].replace(/\s*/g,'');
-    recordString[1] = recordString[1].replace(/\s*/g,'');
-    record.value = recordString[1].replace(/^[\'|\"](\S*)[\'|\"]$/,'$1');
-    record.id = recordString[0];
+    if(recordString.charCodeAt(8) != 61 || recordString.charCodeAt(9) != 32){ // Value indicator Sec 4.1.2.2
+      error("Error in header record. The keyword must be followed by the '=' character and one blank space. It might be that the keyword is larger than 8 characters");
+    } 
+  
+    record.keyWord = keyWord;
     return record;
   }
   
-  function parseHeaderBlock(blockString){
-     var records = {};
+  function parseHeaderBlock(blockString, error, warning){
+     var records = [];
      var record = {};
      var bytePointer = 0;
      var recordString;
      var previousRecordId;
      while(bytePointer < blockString.length){
-         recordString = blockString.substring(bytePointer, bytePointer + recordSize - 1);
-         if(/^END[\x20]*/.test(recordString)){
-           records.end = true;
-           return records;
-         }
-         //console.log(recordString);
-         bytePointer+=recordSize;
-         record = parseHeaderRecord(recordString);
-         if(record){
-           if(record.id){
-             previousRecordId = record.id;
-             records[record.id] = record.value;
-           }
-           else{ // It's a comment record
-           records[previousRecordId].comment += record.comment; 
-         }
+       recordString = blockString.substring(bytePointer, bytePointer + recordSize - 1);
+       if(/^END[\x20]*/.test(recordString)){
+         records.end = true;
+         return records;
        }
+       console.log(recordString);
+       bytePointer += recordSize;
+       record = parseHeaderRecord(recordString, error, warning);
+       if(record){
+         records.push(record);
+       }  
      }
     return records;
   }
   
-  FITS.FileReader = function(){
+  FITS.FileParser = function(){
     var file;
-    var headerRecords = {};
     var data = [];
+    var headerRecords = [];
     var fileBytePointer = 0;
     var slice;
     
@@ -104,34 +86,68 @@
       // For Mozilla 4.0+ || Chrome and Safari || Opera and standard browsers
     }
 
-    function readBlock(){
+    function parseHeaderBlocks(success, error){
       var fileBlock;
-      var reader = new FileReader(); 
+      var reader = new FileReader();
+      var records = headerRecords;
+      
+      var parseError = function(message){
+        error("Error parsing file: " + message);
+      };
+      
+      var parseWarning = function(message){
+        error("Warning: " + message);
+      };
+       
       reader.onload = function(e) {
-        var parsedHeaderRecords;
+        var parsedRecords;
         // Checking allowed characters in Header Data Unit (HDU). 
         // Subset of ASCII characters between 32 and 126 (20 and 7E in hex)
-        if(!/[\x20-\x7E]*/.test(this.result)){
-          console.error("Ilegal character in header block")
+        if(!/^[\x20-\x7E]*$/.test(this.result)){ // Sec 3.2
+          error("Ilegal character in header block")
         }
-        parsedHeaderRecords = parseHeaderBlock(this.result);
-        headerRecords = extend(headerRecords, parsedHeaderRecords);
-        if(!parsedHeaderRecords.end){
-          readBlock();
+        parsedRecords = parseHeaderBlock(this.result, parseError, parseWarning);
+        records = extend(records, parsedRecords);
+        if(parsedRecords.errorMessage){
+          parseError(parsedRecords.errorMessage);
+        }
+        if(!parsedRecords.end){
+          parseHeaderBlocks(success, error);
+        }
+        else{
+          //parseDataBlocks();
+          success(records, data); 
         }
       };
 
       reader.onerror = function(e) {
-        console.error("Error reading file");
+        console.error("Error loading block");
       };
+      
+      if(fileBytePointer == blockSize){ // After reading the first block
+        if(headerRecords[0].keyWord != 'SIMPLE'){  
+          parseError('First keyword in primary header must be SIMPLE'); // Sec 4.4.1.1
+        }
+        else{
+          if(headerRecords[0].value == 'F'){  
+            parseWarning("This file doesn't conform the standard"); // Sec 4.4.1.1
+          }
+          else {
+            if(headerRecords[0].value != 'T'){  
+              parseError('First value in primary header must be T'); // Sec 4.4.1.1
+            }
+          }
+        }
+      }
       
       fileBlock = slice.call(file, fileBytePointer, fileBytePointer + blockSize);
       fileBytePointer += blockSize;
       reader.readAsText(fileBlock);
+      
     }
    
-    this.read = function(inputFile){
-      headerRecords = {};
+    this.parse = function(inputFile){
+      headerRecords = [];
       data = [];
       fileBytePointer = 0;
       file = inputFile;
@@ -139,10 +155,13 @@
         console.error('Failed when loading file. No file selected');
         return;
       }
-      readBlock();
+      parseHeaderBlocks(this.onParsed, this.onError);
     };
     
     this.onParsed = function(header, data){};
+    this.onError = function(error){
+      console.error(error);
+    };
 
   }
 
