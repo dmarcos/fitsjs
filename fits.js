5f9f2df739d8ace6f119cd2be53d153b597ec62e
Diego Marcos <diego.marcos@gmail.com>
2011-09-15 17:43:12 -0700

    Adding mandatory keywords validator

--- fits.js
+++ fits.js
@@ -1,6 +1,7 @@
 // FITS Standard 3.0
 
-(function(){
+(function () {
+  "use strict";
 
   // Save a reference to the global object.
   var root = this;
@@ -17,84 +18,167 @@
   
   var blockSize = 2880; // In bytes
   var recordSize = 80;
-  var mandatoryKeywordsPrimaryHeader = ['BITPIX','NAXIS'];  
-  var mandatoryKeywordsExtensions = [];
-  var fixedFormatKeywords = {
-    BITPIX: {
-      valueType: "integer",
-      validValues: [8,16,32,64,-32,-64]
-    },
-    NAXIS: {
-     valueType: "integer",
-     validValues: { min: 0, max: 999} 
-    }
-  };
+  var stringValue = /^\x27[\x20-\x7E]*\x27$/;
+  var mandatoryKeywordsPrimaryHeader = ['BITPIX', 'NAXIS'];  // Sec 4.4.1.1
+  var mandatoryKeywordsExtensions = ['XTENSION', 'BITPIX', 'NAXIS', 'PCOUNT', 'GCOUNT']; // Sec 4.4.1.2
   
-  function extend(objTarget, objSource){
-    for (var prop in objSource) {
-      if (objSource[prop] !== void 0) objTarget[prop] = objSource[prop];
+  function validateLogical(value, error) {
+    if (!/^(T|F)$/.test(value)) {
+      error('Logical value: ' + value + 'not valid. Must be T or F');
+    } 
+  }
+  
+  function validateDate(value, error) {
+  }
+  
+  function validateInteger(value, error) {
+  }
+  
+  function validateString(value, error) { // Sec 4.2.1
+    if (!stringValue.test(value)) {
+      error('String ' + value + ' contains non valid characters. Sec 4.2.1');
+      return false;
+    }
+    if (value.length > 68) {
+      error('String ' + value + 'too long. Limit is 68 characters. Sec 4.2.1');
+      return false;
+    }
+    value.replace(/\x27{2}/g, "'"); // Replace two sucesive quotes with a single one.
+    return value;
+  }
+  
+  function validateBITPIX(value, error) {
+  }
+  
+  function validateNAXIS(value, error) {
+  }
+  
+  function validatePrimaryHeader() {
+  }
+  
+  function validateExtensionHeader() {
+  }
+  
+  function extend(objTarget, objSource) {
+    var prop;
+    for (prop in objSource) {
+      if (objSource[prop] !== void 0) {
+        objTarget[prop] = objSource[prop];
+      }
     }
     return objTarget; 
   }
   
-  if (!String.prototype.trim) {
-    String.prototype.trim = function () {
-      return this.replace(/^\s*(\S*(?:\s+\S+)*)\s*$/, "$1");
-    };
-  }
+  var fixedFormatKeywords = {
+    BITPIX: { // Sec 4.4.1.1
+      type: "integer",
+      validValues: [8, 16, 32, 64, -32, -64],
+      validate: validateBITPIX
+    },
+    NAXIS: { // Sec 4.4.1.1
+      type: "integer",
+      validValues: { min: 0, max: 999 },
+      validate: validateNAXIS
+    },
+    PCOUNT: {
+      type: "integer",
+      validate: validateInteger
+    },
+    GCOUNT: {
+      type: "integer",
+      validate: validateInteger
+    },
+    DATE: {
+      type: "date",
+      validate: validateDate 
+    },
+    ORIGIN: {
+      type: "string",
+      validate: validateString 
+    },
+    EXTEND: {
+      type: "logical",
+      validate: validateLogical
+    }
+  };
   
-  function parseHeaderRecord(recordString, error, warning){
+  var trim = function (inputString, leading, trailing) {
+    var trimmedString = inputString;
+    if (!leading && !trailing) {
+      return trimmedString.replace(/^\s*(\S*(?:\s+\S+)*)\s*$/, "$1");
+    } else {
+      if (leading) {
+        trimmedString = trimmedString.replace(/^\s*(\S*(?:\s+\S+)*\s*)$/, "$1");
+      }
+      if (trailing) {
+        trimmedString = trimmedString.replace(/^(\s*\S*(?:\s+\S+)*)\s*$/, "$1");
+      }
+      return trimmedString;
+    }
+  };
+
+  function parseHeaderRecord(recordString, error, warning) {
     var record = {};
-    var valueComment = recordString.substring(10);
+    var valueCommentExpr = /^(\s*\x27.*\x27\s*|[^\x2F]*)\x2F{0,1}(.*)$/;
+    var stringExpr = /^\s*\x27.*\x27\s*$/;
+    var valueComment = valueCommentExpr.exec(recordString.substring(10));
     var value;
     var comment;
-    var keyWord = recordString.substring(0, 8); // Keyword in the first 8 bytes. Sec 4.1.2.1
-    keyWord = keyWord.trim();
-    if(keyWord&&!/^[\x30-\x39\x41-\x5A\x5F\x2D]+$/.test(keyWord)){ // Allowed characters in keyword Sec 4.1.2.1
-      error("Illegal characther in header keyWord " + keyWord);
+    var keyword = recordString.substring(0, 8); // Keyword in the first 8 bytes. Sec 4.1.2.1
+    keyword = trim(keyword);
+    if (keyword && !/^[\x30-\x39\x41-\x5A\x5F\x2D]+$/.test(keyword)) { // Allowed characters in keyword Sec 4.1.2.1
+      error("Illegal characther in header keyword " + keyword);
     }
-    if(recordString.charCodeAt(8) != 61 || recordString.charCodeAt(9) != 32){ // Value indicator Sec 4.1.2.2
-      comment = recordString.substring(8).trim(); // If not value all the rest of the record treated like a comment Sec 4.1.2.3
-    }
-    else{ 
-      valueComment = valueComment.split(/\//);
-      value = valueComment[0].trim();
-      if(valueComment[1]){
-        comment = valueComment[1].trim();
-        if(!/^[\x20-\x7E]*$/.test(comment)){ // Allowed characters in comments Sec 4.1.2.3
-          warning("Illegal characther in record comment for record " + keyWord);
+    if (recordString.charCodeAt(8) !== 61 || recordString.charCodeAt(9) !== 32) { // Value indicator Sec 4.1.2.2
+      comment = trim(recordString.substring(8)); // If not value all the rest of the record treated like a comment Sec 4.1.2.3
+    } else {
+      value = trim(valueComment[1]) || undefined;
+      comment = trim(valueComment[2]) || undefined;
+      if (value) {
+        if (stringExpr.test(value)) {
+          if (fixedFormatKeywords[keyword]) {
+            if (recordString.charCodeAt(10) !== 39) {
+              error("Illegal characther in header keyword " + keyword + " Fixed format keyword values must start with simple quote after ="); // Sec 4.2.1
+            }
+          }
+          value = validateString(value, error).replace(/\x27/g, '');
+          value = trim(value, false, true); // Removing non significant trailing spaces. Sec. 4.2.1 
+        }
+      }
+      if (comment) {
+        if (!/^[\x20-\x7E]*$/.test(comment)) { // Allowed characters in comments Sec 4.1.2.3
+          warning("Illegal characther in record comment for record " + keyword);
         }
       }
     }
-    record.keyWord = keyWord;
+    record.keyword = keyword;
     record.comment = comment;
     record.value = value;
     return record;
   }
   
-  function parseHeaderBlock(blockString, error, warning){
-     var records = [];
-     var record = {};
-     var bytePointer = 0;
-     var recordString;
-     var previousRecordId;
-     while(bytePointer < blockString.length){
-       recordString = blockString.substring(bytePointer, bytePointer + recordSize - 1);
-       if(/^END[\x20]*/.test(recordString)){
-         records.end = true;
-         return records;
-       }
-       console.log(recordString);
-       bytePointer += recordSize;
-       record = parseHeaderRecord(recordString, error, warning);
-       if(record){
-         records.push(record);
-       }  
-     }
+  function parseHeaderBlock(blockString, error, warning) {
+    var records = [];
+    var record = {};
+    var bytePointer = 0;
+    var recordString;
+    while (bytePointer < blockString.length) {
+      recordString = blockString.substring(bytePointer, bytePointer + recordSize - 1);
+      if (/^END[\x20]*/.test(recordString)) {
+        records.end = true;
+        return records;
+      }
+      console.log(recordString);
+      bytePointer += recordSize;
+      record = parseHeaderRecord(recordString, error, warning);
+      if (record) {
+        records.push(record);
+      }  
+    }
     return records;
   }
   
-  FITS.FileParser = function(){
+  FITS.FileParser = function () {
     var file;
     var data = [];
     var headerRecords = [];
@@ -105,58 +189,53 @@
     if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
       console.error('The File APIs are not fully supported in this browser.');
       return;
-    }
-    else{
+    } else {  // For Mozilla 4.0+ || Chrome and Safari || Opera and standard browsers
       slice = File.prototype.mozSlice || File.prototype.webkitSlice || File.prototype.slice;
-      // For Mozilla 4.0+ || Chrome and Safari || Opera and standard browsers
     }
 
-    function parseHeaderBlocks(success, error){
+    function parseHeaderBlocks(success, error) {
       var fileBlock;
       var reader = new FileReader();
       
-      var parseError = function(message){
+      var parseError = function (message) {
         error("Error parsing file: " + message);
       };
       
-      var parseWarning = function(message){
+      var parseWarning = function (message) {
         error("Warning: " + message);
       };
        
-      reader.onload = function(e) {
+      reader.onload = function (e) {
         var parsedRecords;
         // Checking allowed characters in Header Data Unit (HDU). 
         // Subset of ASCII characters between 32 and 126 (20 and 7E in hex)
-        if(!/^[\x20-\x7E]*$/.test(this.result)){ // Sec 3.2
-          error("Ilegal character in header block")
+        if (!/^[\x20-\x7E]*$/.test(this.result)) { // Sec 3.2
+          error("Ilegal character in header block");
         }
         parsedRecords = parseHeaderBlock(this.result, parseError, parseWarning);
         headerRecords = [].concat(headerRecords, parsedRecords);
-        if(parsedRecords.errorMessage){
+        if (parsedRecords.errorMessage) {
           parseError(parsedRecords.errorMessage);
         }
-        if(!parsedRecords.end){
+        if (!parsedRecords.end) {
           parseHeaderBlocks(success, error);
-        }
-        else{
+        } else {
           success(headerRecords); 
         }
       };
 
-      reader.onerror = function(e) {
+      reader.onerror = function (e) {
         console.error("Error loading block");
       };
       
-      if(fileBytePointer == blockSize){ // After reading the first block
-        if(headerRecords[0].keyWord != 'SIMPLE'){  
+      if (fileBytePointer === blockSize) { // After reading the first block
+        if (headerRecords[0].keyword !== 'SIMPLE') {  
           parseError('First keyword in primary header must be SIMPLE'); // Sec 4.4.1.1
-        }
-        else{
-          if(headerRecords[0].value == 'F'){  
+        } else {
+          if (headerRecords[0].value === 'F') {  
             parseWarning("This file doesn't conform the standard"); // Sec 4.4.1.1
-          }
-          else {
-            if(headerRecords[0].value != 'T'){  
+          } else {
+            if (headerRecords[0].value !== 'T') {  
               parseError('First value in primary header must be T'); // Sec 4.4.1.1
             }
           }
@@ -169,26 +248,27 @@
       
     }
     
-    function parseDataBlocks(success, error){
+    function parseDataBlocks(success, error) {
       var data = [];
       success(data);
     }
     
-    function parseHeaderDataUnit(success, error){
+    function parseHeaderDataUnit(success, error) {
       var header;
       var extensions;
-      var succesParsingHeader = function(headerRecords){
+      var successParsingData = function (data) {
+        success({'header': header, 'data': data});
+      };
+      
+      var succesParsingHeader = function (headerRecords) {
         header = headerRecords;
         parseDataBlocks(successParsingData, error);
       };
       
-      var successParsingData = function(data){
-        success({'header': header, 'data': data});
-      };
-      parseHeaderBlocks(succesParsingHeader,error);
+      parseHeaderBlocks(succesParsingHeader, error);
     }
    
-    this.parse = function(inputFile){
+    this.parse = function (inputFile) {
       headerRecords = [];
       data = [];
       fileBytePointer = 0;
@@ -200,11 +280,11 @@
       parseHeaderDataUnit(this.onParsed, this.onError); 
     };
     
-    this.onParsed = function(header, data){};
-    this.onError = function(error){
+    this.onParsed = function (header, data) {};
+    this.onError = function (error) {
       console.error(error);
     };
 
-  }
+  };
 
 }).call(this);
