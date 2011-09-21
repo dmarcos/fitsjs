9f3d6a46cdfdba5d13a42851f95efe80b3e1d9c6
Diego Marcos <diego.marcos@gmail.com>
2011-09-20 17:20:39 -0700

    Forgot to commit file

---
+++ binaryDataView.js
@@ -0,0 +1,90 @@
+// DataView API wrapper. String -> ArrayBuffer converter
+// Author: Diego Marcos
+// Email: diego.marcos@gmail.com
+
+(function () {
+
+  // Save a reference to the global object.
+   var root = this;
+
+  // The top-level namespace. Exported for both CommonJS and the browser.
+  var FITS;
+  if (typeof exports !== 'undefined') {
+    FITS = exports;
+  } else {
+    FITS = root.FITS = root.FITS || {};
+  }
+
+  FITS.BinaryDataView = function(binaryData, plittleEndian){
+    
+    var littleEndian = littleEndian === undefined ? true : littleEndian;
+    var dataBuffer;
+    var dataView;
+    var bytePointer = 0;
+    var bufferLength;
+    var dataSize = {
+      'Int8': 1,
+    	'Int16': 2,
+    	'Int32': 4,
+    	'Uint8': 1,
+    	'Uint16': 2,
+    	'Uint32': 4,
+    	'Float32': 4,
+    	'Float64': 8
+    };
+    
+    var dataGetter = function(byteOffset, plittleEndian, type){
+      var data;
+      if(plittleEndian === undefined){
+        plittleEndian = littleEndian;
+      }
+      if(!byteOffset){
+        byteOffset = bytePointer;
+      }
+      data = dataView['get' + type](byteOffset, plittleEndian); 
+      bytePointer = byteOffset + dataSize[type];
+      return data;
+    }
+    
+    if (!window.ArrayBuffer || !window.DataView) {
+      throw new Error('The ArrayBuffer and DataView APIs are not supported in your browser.');
+    }
+    
+    function parseBinaryString(binaryString){
+      var i = 0;
+      var character; 
+      var byte;
+      bufferLength = binaryString.length;
+      dataBuffer = new ArrayBuffer(binaryString.length);
+      dataView = new DataView(dataBuffer, 0, bufferLength);
+      //dataView = new Uint8Array(dataBuffer);
+      while (i < binaryString.length) {
+        character = binaryString.charCodeAt(i);
+        byte = character & 0xff;  
+        dataView.setUint8(i, byte);
+        i += 1;
+      }
+    }
+    
+    if (typeof binaryData === 'string') {
+      parseBinaryString(binaryData)
+    } else {
+      if (binaryData instanceof ArrayBuffer) {
+        dataBuffer = binaryData;
+        bufferLength = dataBuffer.length;
+        dataView = new DataView(dataBuffer, 0, bufferLength);
+      }
+    }
+    
+    this.getInt8 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Int8') };
+    this.getInt16 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Int16') };
+    this.getInt32 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Int32') };
+    this.getUint8 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Uint8'); };    
+    this.getUint16 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Uint16'); };    
+    this.getUint32 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Uint32'); };    
+    this.getFloat32 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Float32'); };    
+    this.getFloat64 = function(byteOffset, littleEndian) { return dataGetter(byteOffset, littleEndian, 'Float64'); };    
+  
+  };
+
+}).call(this);
