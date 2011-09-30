6d3e1ae60cf071a0f8f9a63d666355d789e34204
Diego Marcos <diego.marcos@gmail.com>
2011-09-29 18:31:52 -0700

    Adding Panning + Zooming

--- fits.js
+++ fits.js
@@ -14,10 +14,16 @@
   
   var offScreenCanvas;
   var onScreenCanvas;
+  var offScreenContext;
+  var offScreenWidth;
+  var offScreenHeight;
+  var onScreenContext;
   var viewportPosition = { x : 0, y : 0 };
+  var lastScrollPosition = {};
   var viewportWidth;
   var viewportHeight;
   var mouseDown = false;
+  var zoomFactor = 1;
   
   function renderPixels(pixels, canvas){
     var context = canvas.getContext("2d");
@@ -37,32 +43,59 @@
   }
   
   function draw(){
-    var offScreenContext = offScreenCanvas.getContext('2d');
-    var onScreenContext = onScreenCanvas.getContext('2d');
-    var pixels = offScreenContext.getImageData(viewportPosition.x, viewportPosition.y, viewportWidth, viewportHeight);
-    onScreenContext.putImageData(pixels, 0, 0);
+    // onScreenContext.putImageData(offScreenCanvas.getImageData(viewportPosition.x, viewportPosition.y, viewportWidth, viewportHeight), 0, 0);
+    var widthClip = offScreenWidth >= viewportWidth? viewportWidth : offScreenWidth;
+    var heightClip = offScreenHeight >= viewportHeight? viewportHeight : offScreenHeight;
+    onScreenContext.clearRect(0, 0, viewportWidth, viewportHeight) 
+    onScreenContext.drawImage(offScreenCanvas, viewportPosition.x, viewportPosition.y, widthClip / zoomFactor, heightClip / zoomFactor, 0, 0, widthClip, heightClip);
   }
   
   function mouseMoved(event){
+    var scrollVector;
+    var mousePosition;
     if (mouseDown) {
-      if (event.layerX || event.layerX == 0) { // Firefox
-        viewportPosition.x = event.layerX;
-        viewportPosition.y = event.layerY;
-      } else if (event.offsetX || event.offsetX == 0) { // Opera
-        viewportPosition.x = event.offsetX;
-        viewportPosition.y = event.offsetY;
+      scrollVector = {};
+      mousePosition = {};
+      mousePosition.x = event.layerX || event.offsetX; 
+      mousePosition.y = event.layerY || event.offsetY;
+      scrollVector.x = lastScrollPosition.x - mousePosition.x; 
+      scrollVector.y = lastScrollPosition.y - mousePosition.y;
+      if (viewportPosition.x + scrollVector.x >= 0 && 
+          viewportPosition.x + scrollVector.x + viewportWidth <= offScreenWidth ) {
+            viewportPosition.x = viewportPosition.x + scrollVector.x / zoomFactor;
+            lastScrollPosition.x = mousePosition.x;
       }
-      console.log(viewportPosition.x + " " + viewportPosition.y);
+          
+      if(viewportPosition.y + scrollVector.y >= 0 && 
+         viewportPosition.y + scrollVector.y + viewportHeight <= offScreenHeight ) {
+           viewportPosition.y = viewportPosition.y + scrollVector.y / zoomFactor;
+           lastScrollPosition.y = mousePosition.y;
+      }
       draw();
     }
   }
   
-  var buttonPressed = function(){
+  var buttonPressed = function(event){
     mouseDown = true;
+    lastScrollPosition.x = event.layerX || event.offsetX;
+    lastScrollPosition.y = event.layerY || event.offsetY;
   }
   
   var buttonReleased = function(){
     mouseDown = false;
+  }
+  
+  var mouseOut = function(){
+    mouseDown = false;
+  }
+  
+  var wheelMoved = function (event){
+    var wheel = event.wheelDelta/120;//n or -n
+    var newZoomFactor = wheel > 0? zoomFactor*2 : zoomFactor/2;
+    if (newZoomFactor >= 1) {
+      zoomFactor = newZoomFactor; 
+      draw();
+    }
   }
   
   FITS.renderFile = function(file, canvas, success){
@@ -71,10 +104,16 @@
     canvas.onmousedown = buttonPressed;
     canvas.onmouseup = buttonReleased;
     canvas.addEventListener('mousemove', mouseMoved, false);
+    canvas.addEventListener('mouseout', mouseOut, false);
+    canvas.addEventListener('mousewheel', wheelMoved, false);
+
     offScreenCanvas = document.createElement('canvas');
     onScreenCanvas = canvas;
-    viewportWidth = canvas.getAttribute('width');
-    viewportHeight = canvas.getAttribute('height');
+    onScreenContext = onScreenCanvas.getContext('2d');
+    offScreenContext = offScreenCanvas.getContext('2d');
+    onScreenCanvas.onselectstart = function () { return false; } // ie 
+    viewportWidth = parseInt(canvas.getAttribute('width'));
+    viewportHeight = parseInt(canvas.getAttribute('height'));
     
     fitsParser.onParsed = function(headerDataUnits){
       var HDUs = headerDataUnits;
@@ -83,6 +122,8 @@
       var imageHeight = HDUs[0].header.NAXIS2;
       offScreenCanvas.setAttribute('width', imageWidth);
       offScreenCanvas.setAttribute('height', imageHeight);
+      offScreenWidth = imageWidth;
+      offScreenHeight = imageHeight;
       renderPixels(pixels, offScreenCanvas);
       draw();
       console.log("File read!");
