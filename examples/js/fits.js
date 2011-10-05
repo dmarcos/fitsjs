0820c9a0137c13b3ed64dd3e59caf2a2824fc0d2
Diego Marcos <diego.marcos@gmail.com>
2011-10-04 17:09:54 -0700

    updating example

--- examples/js/fits.js
+++ examples/js/fits.js
@@ -16,49 +16,63 @@
   var offScreenContext;
   var offScreenCanvasWidth;
   var offScreenCanvasHeight;
+
   var onScreenCanvas;
   var onScreenContext;
   var onScreenCanvasWidth;
   var onScreenCanvasHeight;
+
   var viewportPosition = { x : 0, y : 0 };
   var viewportWidth;
   var viewportHeight;
+
   var lastScrollPosition = {};
   var mouseDown = false;
   var zoomFactor = 1;
+
+  var pixelValues = [];
   
   function renderPixels(pixels, canvas){
     var context = canvas.getContext("2d");
     var image = context.createImageData(canvas.getAttribute('width'), canvas.getAttribute('height'));
     var pixelIndex = 0;
     var currentPixel; 
+    pixelValues = [];
     while (pixelIndex < pixels.length) {
       currentPixel = pixels[pixelIndex]; 
       image.data[pixelIndex*4] = currentPixel.red;
       image.data[pixelIndex*4 + 1] = currentPixel.green;
       image.data[pixelIndex*4 + 2] = currentPixel.blue;
       image.data[pixelIndex*4 + 3] = currentPixel.alpha;
+      pixelValues.push(currentPixel.value);
       pixelIndex += 1;
     }
     context.putImageData(image, 0, 0);
   }
+
+  function cursorToPixel(cursorX, cursorY){
+    var viewportPixelX = cursorX / zoomFactor;
+    var viewportPixelY = cursorY / zoomFactor;
+    return {
+      "x" : Math.round(viewportPosition.x + viewportPixelX),
+      "y" : Math.round(viewportPosition.y + viewportPixelY)
+    };
+  }
   
-  function centerViewport(scaleFactor, zoomIn){
-    var scaledViewportWidth = zoomIn? onScreenCanvasWidth / scaleFactor : viewportWidth;
-    var scaledViewportHeight = zoomIn? onScreenCanvasHeight / scaleFactor : viewportHeight;
-    var newX = scaledViewportWidth; 
-    var newY = scaledViewportHeight;
-    if (!zoomIn) {
-      newX = -newX;
-      newY = -newY;
-    }
-    newX = viewportPosition.x + newX / 2; 
-    newY = viewportPosition.y + newY / 2; 
-    if (newX < 0 || newY < 0) {
+  function centerViewport(scaleFactor, zoomIn, cursorX, cursorY){
+    var newPositionX;
+    var newPositionY;
+    var translationX = cursorX / scaleFactor;
+    var translationY = cursorY / scaleFactor;
+    var xOffset = zoomIn? translationX : - translationX / 2; 
+    var yOffset = zoomIn? translationY : - translationY / 2; 
+    newPositionX = viewportPosition.x + xOffset; 
+    newPositionY = viewportPosition.y + yOffset; 
+    if (newPositionX < 0 || newPositionY < 0) {
       return;
     }
-    viewportPosition.x = newX, 
-    viewportPosition.y = newY;
+    viewportPosition.x = newPositionX, 
+    viewportPosition.y = newPositionY;
   }
 
   function scaleViewport(zoomFactor){
@@ -69,13 +83,14 @@
   function draw(){
     // onScreenContext.putImageData(offScreenCanvas.getImageData(viewportPosition.x, viewportPosition.y, viewportWidth, viewportHeight), 0, 0);
     scaleViewport(zoomFactor); 
-    onScreenContext.clearRect(0, 0, viewportWidth, viewportHeight);
+    onScreenContext.clearRect(0, 0, onScreenCanvasWidth, onScreenCanvasHeight);
     onScreenContext.drawImage(offScreenCanvas, viewportPosition.x, viewportPosition.y, viewportWidth, viewportHeight, 0, 0, onScreenCanvasWidth, onScreenCanvasHeight);
   }
   
   function mouseMoved(event){
     var scrollVector;
     var mousePosition;
+    FITS.onPixelChanged(cursorToPixel(event.offsetX, event.offsetY));
     if (mouseDown) {
       scrollVector = {};
       mousePosition = {};
@@ -102,35 +117,54 @@
     mouseDown = true;
     lastScrollPosition.x = event.layerX || event.offsetX;
     lastScrollPosition.y = event.layerY || event.offsetY;
-  }
+  };
   
   var buttonReleased = function(){
     mouseDown = false;
-  }
+  };
   
   var mouseOut = function(){
     mouseDown = false;
-  }
-  
-  var wheelMoved = function (event){
-    var wheel = event.wheelDelta/120;//n or -n
-    var newZoomFactor = wheel > 0? zoomFactor*2 : zoomFactor/2;
+  };
+
+  var zoom = function(newZoomFactor, mouseX, mouseY){
     if (newZoomFactor >= 1 && newZoomFactor < zoomFactor || // Zoom out
         newZoomFactor > zoomFactor && viewportHeight >= 2 && viewportWidth >= 2) { // Zoom In
-      centerViewport(newZoomFactor, newZoomFactor > zoomFactor);    
+      centerViewport(newZoomFactor, newZoomFactor > zoomFactor, mouseX, mouseY);    
       zoomFactor = newZoomFactor; 
       draw();
     }
-  }
+  };
+
+  var doubleClick = function (event) {
+    zoomIn(event.offsetX, event.offsetY);
+  };
+
+  var zoomIn = function(mouseX, mouseY) {
+    zoom(zoomFactor*2, mouseX, mouseY);
+  };
+  
+  var wheelMoved = function (event){
+    var wheel = event.wheelDelta/120;//n or -n
+    zoom(wheel > 0? zoomFactor*2 : zoomFactor/2, event.offsetX, event.offsetY);
+  };
+
+  FITS.onPixelChanged = function(pixelInfo) {
+    console.log("Pixel: " + pixelInfo.x + " " + pixelInfo.y + " " + pixelValues[pixelInfo.x + pixelInfo.y*offScreenCanvasHeight]);  
+  };
   
   FITS.renderFile = function(file, canvas, success){
     var fitsParser = new FITS.FileParser();
+    var canvasContext = canvas.getContext('2d');
     
     canvas.onmousedown = buttonPressed;
     canvas.onmouseup = buttonReleased;
     canvas.addEventListener('mousemove', mouseMoved, false);
     canvas.addEventListener('mouseout', mouseOut, false);
     canvas.addEventListener('mousewheel', wheelMoved, false);
+    canvas.ondblclick = doubleClick;
+
+    canvasContext.clearRect(0, 0, parseInt(canvas.getAttribute('width')), parseInt(canvas.getAttribute('height')));
 
     offScreenCanvas = document.createElement('canvas');
     onScreenCanvas = canvas;
@@ -139,6 +173,8 @@
     onScreenCanvas.onselectstart = function () { return false; } // ie 
     viewportWidth = parseInt(canvas.getAttribute('width'));
     viewportHeight = parseInt(canvas.getAttribute('height'));
+
+    zoomFactor = 1;
     
     fitsParser.onParsed = function(headerDataUnits){
       var HDUs = headerDataUnits;
@@ -161,6 +197,6 @@
       }
     }
     var fitsHeader = fitsParser.parse(file);
-  }
+  };
   
 }).call(this);
